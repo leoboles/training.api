ccc711df0046d962d8bddacced5995673d0e8f9a
Bruno-giehl <bruno.giehl@hotmail.com>
2023-04-25 16:46:35 -0300

    Adicionado EnderecosController Adicionados metodos do Crud

---
+++ training.api/Controllers/EnderecosController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using training.api.Model;
+
+namespace training.api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EnderecosController : ControllerBase
+    {
+        private readonly TrainingContext context;
+
+        public EnderecosController(TrainingContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Endereco>> GetAll()
+        {
+            return Ok(context.Enderecos);
+        }
+
+        [HttpPost("{idPessoa}")]
+        public ActionResult<Endereco> AddEndereco(long idPessoa, string rua, string bairro, string estado, string numero)
+        {
+            Pessoa? pessoa = context.Pessoas.Where(p => p.Id == idPessoa).FirstOrDefault();
+            if (pessoa != null)
+            {
+                Endereco newEndereco = new Endereco();
+
+                newEndereco.Rua = rua;
+                newEndereco.Numero = numero ?? "S/Nº";
+                newEndereco.Bairro = bairro;
+                newEndereco.Estado = estado;
+                newEndereco.IdPessoa = idPessoa;
+                newEndereco.Pessoa = pessoa;
+
+                context.Enderecos.Add(newEndereco);
+                context.SaveChanges();
+                return Ok(newEndereco);
+            }
+            else
+            {
+                return NotFound(pessoa);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<Endereco> UpdateEndereco(long id, string rua, string bairro, string estado, string numero)
+        {
+            Endereco? endereco = context.Enderecos.Where(e => e.Id == id).FirstOrDefault();
+
+            if (endereco != null)
+            {
+                endereco.Rua = rua;
+                endereco.Bairro = bairro;
+                endereco.Numero = numero;
+                endereco.Estado = estado;
+
+                context.Enderecos.Update(endereco);
+                context.SaveChanges();
+                return Ok(endereco);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Endereco> DeleteEndereco(long id)
+        {
+
+            Endereco? endereco = context.Enderecos.Where(e => e.Id == id).FirstOrDefault();
+
+            if (endereco != null)
+            {
+                context.Enderecos.Remove(endereco);
+                context.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
--- training.api/Controllers/PessoasController.cs
+++ training.api/Controllers/PessoasController.cs
@@ -15,9 +15,70 @@ namespace training.api.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<Pessoa>> GetAll()
+        public ActionResult<IEnumerable<Pessoa>> GetAll(string? nome = null, Sexo? sexo = null) 
         {
-            return Ok(context.Pessoas);
+            IQueryable<Pessoa> pessoas = context.Pessoas;
+            if (nome != null)
+                pessoas = pessoas.Where(p => p.Nome.Contains(nome));
+
+            if (sexo != null)
+                pessoas = pessoas.Where(p => p.Sexo == sexo);
+
+            return Ok(pessoas);
         }
+
+        [HttpPost]
+        public ActionResult<Pessoa> AddPessoa(string nome, Sexo sexo, string? telefone = null)
+        {
+            Pessoa newPessoa = new Pessoa();
+
+            newPessoa.Nome = nome;
+            newPessoa.Sexo = sexo;
+            newPessoa.Telefone = telefone;
+
+            context.Pessoas.Add(newPessoa);
+            context.SaveChanges();
+            return Ok(newPessoa);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Pessoa> DeletePessoa(long id)
+        {
+
+            Pessoa? pessoa = context.Pessoas.Where(p => p.Id == id).FirstOrDefault();
+
+            if (pessoa != null)
+            {
+                context.Pessoas.Remove(pessoa);
+                context.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        [HttpPut("{id}")]
+        public ActionResult<Pessoa> UpdatePessoa(long id, string nome, Sexo sexo, string? telefone = null)
+        {
+
+            Pessoa? pessoa = context.Pessoas.Where(p => p.Id == id).FirstOrDefault();
+
+            if (pessoa != null)
+            {
+                pessoa.Nome = nome;
+                pessoa.Sexo = sexo;
+                pessoa.Telefone = telefone;
+
+                context.Pessoas.Update(pessoa);
+                context.SaveChanges();
+                return Ok(pessoa);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
     }
 }
