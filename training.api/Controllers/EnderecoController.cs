475a42ed6984aa1bd16f802aedbb20b42dd91a43
ftwammes <wammesfernanda@gmail.com>
2023-04-25 16:56:03 -0300

    Realizado métodos crud para Pessoa e Endereço

---
+++ training.api/Controllers/EnderecoController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Security.Cryptography;
+using training.api.Model;
+
+namespace training.api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EnderecoController : ControllerBase
+    {
+        private readonly TrainingContext context;
+
+        public EnderecoController(TrainingContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Endereco>> GetAll(string? rua = null, string? numero = null, string? bairro = null, string? estado = null)
+        {
+            IQueryable<Endereco> enderecos = context.Enderecos;
+            if (rua != null)
+            {
+                enderecos = enderecos.Where(e => e.Rua.Contains(rua));
+            }
+            if (numero != null)
+            {
+                enderecos = enderecos.Where(e => e.Numero.Contains(numero));
+
+            }
+            if (bairro != null)
+            {
+                enderecos = enderecos.Where(e => e.Bairro.Contains(bairro));
+            }
+            if (estado != null)
+            {
+                enderecos = enderecos.Where(e => e.Estado.Contains(estado));
+            }
+            return Ok(enderecos);
+        }
+        [HttpGet("{idPessoa}")]
+        public ActionResult<IEnumerable<Endereco>> GetEnderecoByIdPessoa(long idPessoa)
+        {
+            IQueryable<Endereco> endereco = context.Enderecos;
+            if (idPessoa > 0)
+            {
+                endereco = endereco.Where(x => x.IdPessoa == idPessoa);
+            }
+            return Ok(endereco);
+        }
+
+        [HttpPost]
+        public ActionResult<Endereco> CreateEndereco(string rua, string numero, string bairro, string estado, long idPessoa)
+        {
+            var newEndereco = new Endereco
+            {
+                Rua = rua,
+                Numero = numero,
+                Bairro = bairro,
+                Estado = estado,
+                IdPessoa = idPessoa
+            };
+            context.Enderecos.Add(newEndereco);
+            context.SaveChanges();
+            return Ok(newEndereco);
+        }
+        [HttpDelete]
+        public void DeleteEndereco(long id)
+        {
+            var enderecoDelete = context.Pessoas.Find(id);
+            if (enderecoDelete != null)
+            {
+                context.Pessoas.Remove(enderecoDelete);
+                context.SaveChanges();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<Endereco> UpdateEndereco(long id, string numero, string rua, string estado, string bairro)
+        {
+            var enderecoUpdate = context.Enderecos.Find(id);
+            if (enderecoUpdate != null)
+            {
+                enderecoUpdate.Numero = numero;
+                enderecoUpdate.Rua = rua;
+                enderecoUpdate.Estado = estado;
+                enderecoUpdate.Bairro = bairro;
+                context.Enderecos.Update(enderecoUpdate);
+                context.SaveChanges();
+            }
+            return Ok(enderecoUpdate);
+        }
+    }
+}
--- training.api/Controllers/PessoasController.cs
+++ training.api/Controllers/PessoasController.cs
@@ -15,9 +15,67 @@ namespace training.api.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<Pessoa>> GetAll()
+        public ActionResult<IEnumerable<Pessoa>> GetAll(string? nome = null, Sexo? sexo = null)
         {
-            return Ok(context.Pessoas);
+            IQueryable<Pessoa> pessoas = context.Pessoas;
+            if(nome != null) {
+                pessoas = pessoas.Where(x => x.Nome.Contains(nome));
+            }
+            if(sexo != null)
+            {
+                pessoas = pessoas.Where(x => x.Sexo == sexo);
+            }
+            return Ok(pessoas);
         }
+
+        [HttpGet("{id}")]
+        public ActionResult<IEnumerable<Pessoa>> GetById(long id)
+        {
+            IQueryable<Pessoa> pessoas = context.Pessoas;
+            if (id > 0)
+            {
+                pessoas = pessoas.Where(x => x.Id == id);
+            }
+            return Ok(pessoas);
+        }
+
+        [HttpPost]
+        public ActionResult<Pessoa> CreatePessoa(Sexo sexo, string? nome = null, string? telefone = null)
+        {
+            var pessoa = new Pessoa
+            {
+                Nome = nome,
+                Telefone = telefone,
+                Sexo = sexo
+            };
+            context.Pessoas.Add(pessoa);
+            context.SaveChanges();
+            return Ok(pessoa);
+        }
+        [HttpDelete]
+        public void DeletePessoa(long id)
+        {
+            var pessoaDelete = context.Pessoas.Find(id);
+            if(pessoaDelete != null)
+            {
+                context.Pessoas.Remove(pessoaDelete);
+                context.SaveChanges();
+            }
+        }
+        [HttpPut("{id}")]
+        public ActionResult<Pessoa> UpdatePessoa(long id, string nome, string telefone, Sexo sexo)
+        {
+            var pessoaUpdate = context.Pessoas.Find(id);
+            if (pessoaUpdate != null)
+            {
+                pessoaUpdate.Nome = nome;
+                pessoaUpdate.Telefone = telefone;
+                pessoaUpdate.Sexo = sexo;
+                context.Pessoas.Update(pessoaUpdate);
+                context.SaveChanges();
+            }
+            return Ok(pessoaUpdate);
+        }
+
     }
 }
